d555c3b6630f0d7ffedd9d74fc28dd75e4810abe
techtabu <tabudev@gmail.com>
2020-01-08 00:37:51 -0600

    renamed app file

--- amazon-sqs/sqs-springboot/src/main/java/techtabu/aws/sqs/SQSSpringbootApplication.java
+++ amazon-sqs/sqs-springboot/src/main/java/techtabu/aws/sqs/SQSSpringbootApplication.java
@@ -8,10 +8,10 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 @SpringBootApplication
 @EnableSwagger2
 @Slf4j
-public class SqsSpringbootApplication {
+public class SQSSpringbootApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SqsSpringbootApplication.class, args);
+		SpringApplication.run(SQSSpringbootApplication.class, args);
 	}
 
 }
